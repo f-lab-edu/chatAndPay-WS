4f9dbcae3045e6c614bf876d900c4751b6e164ee
kei-ahn <kean@batontouch.co.kr>
2023-05-27 19:27:57 +0900

    [feat] UUID converter 적용

--- src/main/kotlin/com/chatandpay/ws/chat/entity/ChatRoom.kt
+++ src/main/kotlin/com/chatandpay/ws/chat/entity/ChatRoom.kt
@@ -1,8 +1,23 @@
 package com.chatandpay.ws.chat.entity
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.UUID
+
+
+@Document("chatRoom")
 data class ChatRoom(
     val name: String
 ) {
-    val id: String
-        get() = "room_$name"
+    @Id
+    var id: UUID = UUID.randomUUID()
+
 }
+
+
+//data class ChatRoom(
+//    val name: String
+//) {
+//    val id: String
+//        get() = "room_$name"
+//}
--- src/main/kotlin/com/chatandpay/ws/chat/service/ChatRoomService.kt
+++ src/main/kotlin/com/chatandpay/ws/chat/service/ChatRoomService.kt
@@ -3,21 +3,34 @@ package com.chatandpay.ws.chat.service
 import org.springframework.stereotype.Service
 import com.chatandpay.ws.chat.entity.ChatRoom
 import com.chatandpay.ws.chat.repository.ChatRoomRepository
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.util.*
 
 @Service
 class ChatRoomService(
     private val chatRoomRepository: ChatRoomRepository
 ) {
 
-    fun findAllRoom(): List<ChatRoom> {
-        return chatRoomRepository.findAllRoom()
-    }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    class ChatRoomCreationException(message: String, cause: Throwable) : RuntimeException(message, cause)
 
-    fun findById(id: String): ChatRoom {
-        return chatRoomRepository.findById(id)
+    fun findAllRoom(): List<ChatRoom> {
+        return chatRoomRepository.findAll();
+    }
+    fun findById(id: UUID): ChatRoom? {
+        val optionalChatRoom: Optional<ChatRoom> = chatRoomRepository.findById(id)
+        println(optionalChatRoom);
+        return optionalChatRoom.orElse(null)
     }
 
     fun createRoom(name: String): ChatRoom {
-        return chatRoomRepository.createRoom(name)
+        try {
+            val chatRoom = ChatRoom(name)
+            return chatRoomRepository.save(chatRoom)
+        } catch (e: Exception) {       e.printStackTrace()
+            // 적절한 예외 처리를 수행하거나 예외를 던지는 것이 좋습니다.
+            throw ChatRoomCreationException("Failed to create chat room", e)
+        }
     }
 }
